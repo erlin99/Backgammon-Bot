5c7087c17325cc7eb89105b85a867cd7ce2c2ab0
Er Lin <er.lin.li.99@gmail.com>
2019-02-21 10:53:24 +0000

    Created 2 folders in source, one for board images and another for the dice images. Also changed path in code.

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -17,7 +17,7 @@ public class BoardPanel extends JPanel {
     public BoardPanel() 
     {
         try {
-            image = ImageIO.read(getClass().getResource("boardRed.png"));
+            image = ImageIO.read(getClass().getResource("boards/boardRed.png"));
         } catch (IOException ex) {
             // handle exception...
             System.out.println(ex);
